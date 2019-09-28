4b3a4dce9c5ad273d598c64b080003f3a603013a
pawel-antoniuk <pawelantoniuk@outlook.com>
2019-09-28 21:24:51 +0200

    Add nossl option to compose.py

--- tools/compose.py
+++ tools/compose.py
@@ -1,27 +1,34 @@
 import subprocess
 import sys
 
+COMPOSE_COMMAND=['docker-compose', '--project-directory', '.']
+COMPOSE_BASE='compose/compose-base.yml'
+COMPOSE_DEV='compose/compose-dev.override.yml'
+COMPOSE_DEPLOY='compose/compose-deploy.override.yml'
+COMPOSE_SSL='compose/compose-ssl.override.yml'
+ERROR_MSG_FIRST_ARG_EXCEPTED='First argument is excepted to be dev/deploy/both/dev-ssl/deploy-ssl/both-ssl'
+
 if len(sys.argv) < 2:
-    print('dev/deploy as second argument excepted')
+    print(ERROR_MSG_FIRST_ARG_EXCEPTED)
     exit(1)
 
 try:
     if sys.argv[1] == 'dev':
-        subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:])
+        subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_DEV] + sys.argv[2:])
     elif sys.argv[1] == 'deploy':
-        subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
+        subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_DEPLOY] + sys.argv[2:])
     elif sys.argv[1] == 'both':
-        if subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:]).returncode == 0:
-            subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
+        if subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_DEV] + sys.argv[2:]).returncode == 0:
+            subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_DEPLOY] + sys.argv[2:])
     elif sys.argv[1] == 'dev-ssl':
-        subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:])
+        subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_SSL, '-f', COMPOSE_DEV] + sys.argv[2:])
     elif sys.argv[1] == 'deploy-ssl':
-        subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
+        subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_SSL, '-f', COMPOSE_DEPLOY] + sys.argv[2:])
     elif sys.argv[1] == 'both-ssl':
-        if subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:]).returncode == 0:
-            subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
+        if subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_SSL, '-f', COMPOSE_DEV] + sys.argv[2:]).returncode == 0:
+            subprocess.run(COMPOSE_COMMAND + ['-f', COMPOSE_BASE, '-f', COMPOSE_SSL, '-f', COMPOSE_DEPLOY] + sys.argv[2:])
     else:
-        print('dev/deploy as second argument excepted')
+        print(ERROR_MSG_FIRST_ARG_EXCEPTED)
         exit(1)
 except KeyboardInterrupt:
     print('be patient...')
