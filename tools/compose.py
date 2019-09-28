14927c9e5aec7f7350dd51e620f100a0cf8d509f
pawel-antoniuk <pawelantoniuk@outlook.com>
2019-09-28 20:54:37 +0200

    Create volumes directory

--- tools/compose.py
+++ tools/compose.py
@@ -13,6 +13,13 @@ try:
     elif sys.argv[1] == 'both':
         if subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:]).returncode == 0:
             subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
+    elif sys.argv[1] == 'dev-ssl':
+        subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:])
+    elif sys.argv[1] == 'deploy-ssl':
+        subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
+    elif sys.argv[1] == 'both-ssl':
+        if subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-dev.override.yml'] + sys.argv[2:]).returncode == 0:
+            subprocess.run(['docker-compose',  '-f', 'compose-base.yml', '-f', 'compose-ssl.override.yml', '-f', 'compose-deploy.override.yml'] + sys.argv[2:])
     else:
         print('dev/deploy as second argument excepted')
         exit(1)
