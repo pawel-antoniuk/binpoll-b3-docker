e6fb6aa65b9b86def92c0daef8c9c4a095a2e674
pawel-antoniuk <pawelantoniuk@outlook.com>
2019-10-05 20:02:18 +0200

    Remove input and output files from generate_test_audio_set tool

--- tools/test/test_available.py
+++ tools/test/test_available.py
@@ -4,28 +4,35 @@ import json
 
 BASE_URL='https://home.antoniuk.pl/api/'
 AVAILABLE_AUDIO_SET_ENDPOINT=BASE_URL+'available_audio_set/'
-SESSION_KEY='sessionid'
-SESSION_VALUE='acwadpvo98fe6bbqu0paz85boza38gy3'
-REQUEST_HEADERS={'Cookie': f'{SESSION_KEY}={SESSION_VALUE}'}
+DEBUG_ENDPOINT=BASE_URL+'debug/'
+REQUEST_HEADERS={}
 
-available_audio_sets = {}
-response = requests.get(AVAILABLE_AUDIO_SET_ENDPOINT, headers=REQUEST_HEADERS)
-parsed = response.json()
-for audio_set in parsed:
-    available_audio_sets[audio_set['audioSet']['id']] = {
-        'priority': audio_set['audioSet']['priority'],
-        'is_locked': audio_set['isLocked'],
-        'locked_at': audio_set['lockedAt'],
-    }
+with requests.Session() as session:
+    debug_secret = {'key': 'binpoll@5'}
+    response = session.post(DEBUG_ENDPOINT, data=debug_secret, headers=REQUEST_HEADERS)
+    print(f'DEBUG_ENDPOINT response: {response}')
 
-available_audio_sets = dict(sorted(available_audio_sets.items()))
-locked_audio_sets = {k: v for k, v in available_audio_sets.items() if v['is_locked']}
-unlocked_audio_sets = {k: v for k, v in available_audio_sets.items() if not v['is_locked']}
+    available_audio_sets = {}
+    response = session.get(AVAILABLE_AUDIO_SET_ENDPOINT, headers=REQUEST_HEADERS)
+    try:
+        parsed = response.json()
+        for audio_set in parsed:
+            available_audio_sets[audio_set['audioSet']['id']] = {
+                'priority': audio_set['audioSet']['priority'],
+                'is_locked': audio_set['isLocked'],
+                'locked_at': audio_set['lockedAt'],
+            }
+    except json.JSONDecodeError as e:
+        print(response)
 
-with open('test_available_output.txt', 'w') as f:
-    f.write('########## available_audio_sets ##########\n')
-    f.write(f'count all: \t{len(available_audio_sets)}\n')
-    f.write(f'count locked: \t{len(locked_audio_sets)}\n')
-    f.write(f'count unlocked: \t{len(unlocked_audio_sets)}\n')
-    f.write(f'{json.dumps(available_audio_sets, indent=2)}\n')
+    available_audio_sets = dict(sorted(available_audio_sets.items()))
+    locked_audio_sets = {k: v for k, v in available_audio_sets.items() if v['is_locked']}
+    unlocked_audio_sets = {k: v for k, v in available_audio_sets.items() if not v['is_locked']}
+
+    with open('test_available_output.txt', 'w') as f:
+        f.write('########## available_audio_sets ##########\n')
+        f.write(f'count all: \t{len(available_audio_sets)}\n')
+        f.write(f'count locked: \t{len(locked_audio_sets)}\n')
+        f.write(f'count unlocked: \t{len(unlocked_audio_sets)}\n')
+        f.write(f'{json.dumps(available_audio_sets, indent=2)}\n')
 
--- tools/test/test_retain.py
+++ tools/test/test_retain.py
@@ -5,34 +5,43 @@ import datetime
 
 BASE_URL='https://home.antoniuk.pl/api/'
 RETAIN_ENDPOINT=BASE_URL+'retain/'
-SESSION_KEY='sessionid'
-SESSION_VALUE='acwadpvo98fe6bbqu0paz85boza38gy3'
-REQUEST_HEADERS={'Cookie': f'{SESSION_KEY}={SESSION_VALUE}'}
+DEBUG_ENDPOINT=BASE_URL+'debug/'
+REQUEST_HEADERS={}
 
 audio_retains = {}
 retained_sets = []
 retain_fails = []
-for idx in range(80):
-    response = requests.get(RETAIN_ENDPOINT, headers=REQUEST_HEADERS)
-    parsed = response.json()
-    now = str(datetime.datetime.now(datetime.timezone.utc))
-    if parsed['state'] == 'ok':
-        audio_retains.setdefault(parsed['setId'], []).append({'priority': parsed['priority'], 'retain_date': now})
-        retained_sets.append({'setId': parsed['setId'], 'priority': parsed['priority'], 'retain_date': now})
-    else:
-        retain_fails.append({'retain_date': now})
 
-audio_retains = dict(sorted(audio_retains.items()))
+with requests.Session() as session:
+    debug_secret = {'key': 'binpoll@5'}
+    response = session.post(DEBUG_ENDPOINT, data=debug_secret, headers=REQUEST_HEADERS)
+    print(f'DEBUG_ENDPOINT response: {response}')
 
-with open('test_retain_output.txt', 'w') as f:
-    f.write('########## audio_retains ##########\n')
-    f.write(f'count: {len(audio_retains)}\n')
-    f.write(f'{json.dumps(audio_retains, indent=2)}\n')
+    for idx in range(80):
+        response = session.get(RETAIN_ENDPOINT, headers=REQUEST_HEADERS)
+        if response.status_code != 200:
+            print(f'Retain error: {response}')
+            break
+        
+        parsed = response.json()
+        now = str(datetime.datetime.now(datetime.timezone.utc))
+        if parsed['state'] == 'ok':
+            audio_retains.setdefault(parsed['setId'], []).append({'priority': parsed['priority'], 'retain_date': now})
+            retained_sets.append({'setId': parsed['setId'], 'priority': parsed['priority'], 'retain_date': now})
+        else:
+            retain_fails.append({'retain_date': now})
 
-    f.write('########## retained_sets ##########\n')
-    f.write(f'count: {len(retained_sets)}\n')
-    f.write(f'{json.dumps(retained_sets, indent=2)}\n')
+    audio_retains = dict(sorted(audio_retains.items()))
 
-    f.write('########## retain_fails ##########\n')
-    f.write(f'count: {len(retain_fails)}\n')
-    f.write(f'{json.dumps(retain_fails, indent=2)}\n')
+    with open('test_retain_output.txt', 'w') as f:
+        f.write('########## audio_retains ##########\n')
+        f.write(f'count: {len(audio_retains)}\n')
+        f.write(f'{json.dumps(audio_retains, indent=2)}\n')
+
+        f.write('########## retained_sets ##########\n')
+        f.write(f'count: {len(retained_sets)}\n')
+        f.write(f'{json.dumps(retained_sets, indent=2)}\n')
+
+        f.write('########## retain_fails ##########\n')
+        f.write(f'count: {len(retain_fails)}\n')
+        f.write(f'{json.dumps(retain_fails, indent=2)}\n')
